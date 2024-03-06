2c6861d7cb241df076cc665cea7a6690be42072a
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2024-03-06 23:56:52 +0700

    dot_product_recursive

--- dot_product_recursive.s
+++ dot_product_recursive.s
@@ -1,44 +1,60 @@
 .data
-a:  .word 1, 2, 3, 4, 5      # Array a elements
-b:  .word 6, 7, 8, 9, 10     # Array b elements
-str_line: .string "The dot product is: \n"
+a: .word 1, 2, 3, 4, 5
+b: .word 6, 7, 8, 9, 10
+print_text: .asciiz "The dot product is: "
 
 .text
 main:
-    la a0, a                # Load address of array a
-    la a1, b                # Load address of array b
-    addi a2, x0, 5          # size set to a2
-    jal dot_product_recursive
-    
-    # print_string
-    addi a0, x0, 4
-    la a1, str_line
-    ecall
-    
-    # Print result
-    mv a1, a0               # Move result to a1 for printing
-    addi a0, x0, 1          # Syscall number for print_int
-    ecall
-    
-    # Exit
-    addi a0, x0, 10         
-    ecall
+    # a0 = a, a1 = b, a2 = size
+    la a0 a
+    la a1 b
+    addi a2 x0 5
+    jal dot_product_recursive # dot_project_recursive(a0, a1, a2)
+    j exit
 
 dot_product_recursive:
-    addi t0, x0, 1          
-    beq a2, x0, exit        # Base case: if size == 0, return 0
+    addi sp sp -16 # Prepare Stack Pointer
+    sw ra 0(sp) # Save ra into stack
+    sw a0 4(sp) # Save ra into stack
+    sw a1 8(sp) # Save ra into stack
+    sw a2 12(sp) # Save ra into stack
+    addi t0 x0 1 # t0 = temporary 1
+    bne a2 t0 return # If size != 1 then we will call function recursively
+    # Base Case
+    addi sp sp 16 # Reset stack pointer
+    lw t1 0(a0) # a[0]
+    lw t2 0(a1) # b[0]
+    mul a0 t1 t2 # a[0]*b[0]
+    jr ra
     
-    lw t1, 0(a0)            # Load a[0]
-    lw t2, 0(a1)            # Load b[0]
-    mul t3, t1, t2          # t3 = a[0] * b[0]
-    
-    addi a0, a0, 2          
-    addi a1, a1, 2         
-    addi a2, a2, -1         # size - 1
-    jal dot_product_recursive # Recursive call
-    
-    add a0, a0, t3
-    jal dot_product_recursive # Add current product to result of recursive call
+return:
+    # call dot_product_recursive(a+1, b+1, size-1)
+    addi a0 a0 4 # a + 1
+    addi a1 a1 4 # b + 1
+    addi a2 a2 -1 # size - 1
+    jal dot_product_recursive
+    lw ra 0(sp) # load ra
+    lw t0 4(sp) # load t0 = a
+    lw t1 8(sp) # load t1 = b
+    lw t2 12(sp) # load t2 = size
+    addi sp sp 16 # Reset stack pointer
+    # a[0]*b[0] + dot_product_recursive(a+1, b+1, size-1)
+    lw t3 0(t0) # load value of a[0]
+    lw t4 0(t1) # load value of b[0]
+    mul t5 t3 t4 # a[0]*b[0] 
+    add a0 a0 t5 # dot_product_recursive(a+1, b+1, size-1)
+    jr ra
     
 exit:
-    jr ra                   # Return from function
+    mv t0 a0
+
+    addi a0 x0 4
+    la a1 print_text
+    ecall
+
+    mv a1 t0
+    addi a0 x0 1
+    ecall
+
+    addi a0 x0 10
+    ecall
